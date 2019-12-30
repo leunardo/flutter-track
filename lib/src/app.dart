73c2926b6be5d92a2029e86d13d1b4c14e578f7a
Leonardo de Melo Alves <LAlves6@slb.com>
2019-12-30 17:15:14 -0300

    feat: add loading in todos and change to streambuilder

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,22 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:track/src/models/todo.model.dart';
-import 'package:track/src/resources/todo_service.dart';
 import 'package:track/src/ui/welcome.dart';
-import 'package:provider/provider.dart';
 
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        StreamProvider<List<TodoModel>>.value(value: TodoService().streamTodo())
-      ],
-      child: MaterialApp(
+    return MaterialApp(
         theme: ThemeData.dark(),
         home: Scaffold(
           body: WelcomePage(),
         ),
-      ),
     );
   }
 }
--- lib/src/ui/todo.dart
+++ lib/src/ui/todo.dart
@@ -1,20 +1,26 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 import 'package:track/src/models/todo.model.dart';
+import 'package:track/src/resources/todo_service.dart';
 import 'package:track/src/widgets/todo/todo-add-fab.dart';
 import 'package:track/src/widgets/todo/todo-list.dart';
 
 class TodoPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    var todos = Provider.of<List<TodoModel>>(context) ?? [];
-
     return Scaffold(
       appBar: AppBar(
         title: Text("To Do"),
       ),
-      body: Container(
-        child: TodoList(context, todos)
+      body: StreamBuilder(
+        stream: TodoService().streamTodo(),
+        builder: (BuildContext context, AsyncSnapshot<List<TodoModel>> snapshot) {
+          if (snapshot.hasData) {
+            return TodoList(context, snapshot.data);
+          } else if (snapshot.hasError) {
+            return Text('Error !');
+          }
+          return Center(child: CircularProgressIndicator());
+        },
       ),
       floatingActionButton: TodoAddFab(context)
     );
